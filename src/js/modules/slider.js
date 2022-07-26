d971c8029167d6bc88ea792f1907fe6065f38966
koozoo <meteoramtr@gmail.com>
2022-07-26 22:30:43 +0400

    slider done2

---
+++ src/js/modules/slider.js
@@ -0,0 +1,39 @@
+const slider = (content,triggers) => {
+
+    const slides = document.querySelectorAll(content),
+          btns = document.querySelectorAll(triggers);
+    
+          console.log(slides);
+          //функция скрытия всех слайдов и удаления класса актив у кнопок
+    function hideSlides () {
+        slides.forEach(i => {
+            i.classList.add('slider__item-hide');
+       })
+       btns.forEach(btn => {
+        btn.classList.remove('slider__dot-active');
+       })
+    }  
+         //функция показа слада и добавления класса актив у кнопки
+    function showSlides (i = 0) {
+        slides[i].classList.remove('slider__item-hide');
+        
+        btns[i].classList.add('slider__dot-active');
+        
+    } 
+    function showByTime () {
+        
+    }
+    hideSlides();
+    showSlides();
+
+        //  проходим перебором по массиву, где определяем кнопку и номер в массиве, навешиваем обработчик события и вызываем ф-ии,
+        // и в зависемости куда кликнул пользователь передаем этот номер i в ф-ию showSlides()
+    btns.forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            hideSlides();
+            showSlides(i);
+        });
+    });
+};
+
+export default slider;
